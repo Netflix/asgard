236a6e3124530fccdad3c01893e4e69555f526ba
Jason Gritman <jasongritman@gmail.com>
2012-05-29 16:16:32 -0700

    gh-pages initial checkin

--- src/groovy/com/netflix/asgard/plugin/UserDataProvider.groovy
+++
@@ -1,9 +0,0 @@
-package com.netflix.asgard.plugin
-
-import com.netflix.asgard.UserContext
-
-interface UserDataProvider {
-
-    String buildUserDataForVariables(UserContext userContext, String appName, String autoScalingGroupName,
-            String launchConfigName)
-}
