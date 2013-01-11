132c6af1a31fc683ef8c513b7e2715f730896df9
Joe Sondow <jsondow@netflix.com>
2013-01-11 14:23:51 -0800

    ASGARD-1075 Disable form submit on keyboard enter key
    The default browser behavior that allows the Enter key to submit the
    form is dangerous and usually unnecessary. This danger has been
    uncovered by a user navigating a select2 mutli-select input rapidly
    with the keyboard, trying to add many ELBs to an ASG rapidly. The
    form submitted early because of the Enter key, resulting in an ASG
    that unintentionally lacked security groups.
    
    For certain whitelisted, one-field forms that don't result in backend
    state changes, the Enter key can be allowed to submit the form. That
    makes those forms easier to use. For example, any form that merely
    does a search or a browser redirect.

--- web-app/js/custom.js
+++ web-app/js/custom.js
@@ -586,6 +586,12 @@ jQuery(document).ready(function() {
         // Don't let any ajax responses use browser cache
         jQuery.ajaxSetup({ cache: false });
 
+        // Ignore "Enter" key on forms. It's too easy to press enter again by accident after using enter to make a
+        // select2 choice. This usability flaw can cause production outages.
+        jQuery('form').not('.allowEnterKeySubmit').bind('keydown', function(e) {
+            return e.keyCode !== 13;
+        });
+
         // Decorate the menu buttons that have drop down lists. Do the work that CSS3 isn't ready to do yet.
         jQuery('.menuButton').has('ul').addClass('dropdown');
 
