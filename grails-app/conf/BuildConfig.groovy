60e6ce3f3b62f65635eeef1977ecba576cf35a48
Tomas Lin <tomaslin@gmail.com>
2013-10-17 05:55:26 -0700

    run in forked mode cannot find test classes if previously ran in non-test mode

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -50,7 +50,8 @@ grails.project.fork = [
     // configure settings for the test-app JVM, uses the daemon by default
     // test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
     // configure settings for the run-app JVM
-    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+   // run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    
     // configure settings for the run-war JVM
     war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
     // configure settings for the Console UI JVM
