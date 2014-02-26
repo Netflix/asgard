d0666cefa2abd92c96973a156c2839d3b589f924
Clay McCoy <git@claymccoy.com>
2014-02-26 15:55:32 -0800

    initial content from Yeoman Angular generator

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('asgardApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('asgardApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
